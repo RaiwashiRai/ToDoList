338ee4bc71f4dc2779e8ce1451c2cac65b566f76
RaiwashiRai <shubhamggaikwad2002@gmail.com>
2022-07-26 18:45:52 +0530

    2nd commit

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
+//jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use('view engine', 'ejs')
+app.use('view engine', 'ejs');
 
 app.get("/", function(req, res){
-    res.send("hello");
+    // res.send("hello");
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    if (currentDay == 6 || currentDay == 0){
+        day = "Weekend";
+    }
+    else{
+        day = "Weekday";
+    }
+    // res.render method is used for ejs
+    res.render("list", {kindOfDay : day});
 });
 
 
