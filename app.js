dc9915568bdc8b3cdecc3daa18865c0f102249e8
RaiwashiRai <shubhamggaikwad2002@gmail.com>
2022-07-31 16:18:30 +0530

    3

--- app.js
+++ app.js
@@ -5,25 +5,34 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use('view engine', 'ejs');
+let items = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     // res.send("hello");
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    let today = new Date();
 
-    if (currentDay == 6 || currentDay == 0){
-        day = "Weekend";
-    }
-    else{
-        day = "Weekday";
-    }
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let day = today.toLocaleString("en-US", options);
     // res.render method is used for ejs
-    res.render("list", {kindOfDay : day});
+    res.render("list.ejs", {kindOfDay : day, newListItem: items});
 });
 
+app.post("/", function(req, res){
+    let item = req.body.newListItem_;
 
+    items.push(item);
+
+    res.redirect("/");
+});
 
 app.listen(8000, function(){
     console.log("server is running on port 8000");
