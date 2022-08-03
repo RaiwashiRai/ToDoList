4ec1a682bb15659e96004b0333adb009c5023aae
RaiwashiRai <shubhamggaikwad2002@gmail.com>
2022-08-03 16:48:09 +0530

    4

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = [];
+let studyItems = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
     // res.send("hello");
@@ -23,16 +25,31 @@ app.get("/", function(req, res){
 
     let day = today.toLocaleString("en-US", options);
     // res.render method is used for ejs
-    res.render("list.ejs", {kindOfDay : day, newListItem: items});
+    res.render("list.ejs", {listTitle : day, newListItem: items});
 });
 
 app.post("/", function(req, res){
+
     let item = req.body.newListItem_;
 
-    items.push(item);
+    // console.log(req.body);
 
-    res.redirect("/");
+    if (req.body.list === "Study"){
+        studyItems.push(item);
+        res.redirect("/study");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
+
 });
+
+// study list
+app.get("/study", function(req, res){
+    res.render("list.ejs", {listTitle : "Study", newListItem: studyItems});
+});
+
 
 app.listen(8000, function(){
     console.log("server is running on port 8000");
